6bf73f9385878a49b0f36857614fc3025268125b
Fatima-Zahra BOUHASSOUN <fatimazahrabouhassoun267@gmail.com>
2022-11-30 15:59:53 +0100

    Add app division

--- app/app.js
+++ app/app.js
@@ -9,6 +9,12 @@ class Calculator {
     Substraction(a,b){
         return a - b ;
     }
+    Division(a,b){
+        if( b === 0){
+            return 'can\'t devide by 0' 
+        }
+        return a / b ;   
+    }
 }
 
 module.exports = {
