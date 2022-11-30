a26d4374acef2a2e41acb3eb439b73d294756b7f
Fatima-Zahra BOUHASSOUN <fatimazahrabouhassoun267@gmail.com>
2022-11-30 15:23:20 +0100

    Add test Multiplication

--- test/app.test.js
+++ test/app.test.js
@@ -10,11 +10,35 @@ describe('calculator', function(){
             expect(calculator.Add(1,2)).to.equal(calculator.Add(2,1));
             done();
         })
-        it ('1.Identity elemnt is 0 (a + 0 =  a)', function(done){
+        it ('2.Identity elemnt is 0 (a + 0 =  a)', function(done){
             let calculator = new Calculator();
             expect(calculator.Add(0,2)).to.equal(2);
             done();
         })
+
+    })
+    describe('Multiplication', function(){
+        it ('1.Multiplication is Comutative (a * b = b * a)', function(done){
+            let calculator = new Calculator();
+            expect(calculator.Multiplication(1,2)).to.equal(calculator.Multiplication(2,1));
+            done();
+        })
+        it ('2.Multiply by  0 (a * 0 =  0)', function(done){
+            let calculator = new Calculator();
+            expect(calculator.Multiplication(0,2)).to.equal(0);
+            done();
+        })
+        it ('3.Multiply by  1 (a * 1 = a)', function(done){
+            let calculator = new Calculator();
+            expect(calculator.Multiplication(1,2)).to.equal(2);
+            done();
+        })
+        it ('4.Multiply by negatif number (a * -b  = -x )', function(done){
+            let calculator = new Calculator();
+            expect(calculator.Multiplication(1, -2)).to.equal(-2);
+            done();
+        })
+
 
     })
 })
